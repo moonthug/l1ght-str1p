850a8b80dcb1cec8d6078c79929b5033628ee995
Alex Coulcher <alex@polyglot.rodeo>
2021-02-01 17:42:32 +0000

    feat: Add GME 💎🤚🏻

---
+++ src/scenes/gme.ts
@@ -0,0 +1,80 @@
+import axios, { AxiosResponse } from 'axios';
+import { Canvas } from '../Canvas';
+import { Scene } from './Scene';
+import SimplexNoise from 'simplex-noise';
+
+interface WeatherData {
+  error?: boolean;
+  loading: boolean;
+  simplex: SimplexNoise;
+  value?: {
+    previousClosePrice: number;
+    currentPrice: number;
+  };
+}
+
+interface FinnhubResponse {
+  c: number;
+  h: number;
+  l: number;
+  o: number;
+  pc: number;
+  t: number;
+}
+
+function drawLoading (canvas: Canvas, frame: number) {
+  const position = frame % canvas.getLength();
+  canvas.setPixel(position, [0, 255, 128]);
+  canvas.fill([0, 0, 0, 50]);
+}
+
+export const scene: Scene<WeatherData> = {
+  data: {
+    loading: true,
+    error: false,
+    simplex: new SimplexNoise()
+  },
+  setup () {
+    scene.data.loading = true;
+    axios('https://finnhub.io/api/v1/quote?symbol=GME&token=c0c39tv48v6o915a1ang')
+      .then((response: AxiosResponse<FinnhubResponse>) => {
+        scene.data.error = false;
+        scene.data.loading = false;
+        scene.data.value = {
+          currentPrice: response.data.c,
+          previousClosePrice: response.data.pc
+        };
+      })
+      .catch((e) => {
+        console.log(e);
+        scene.data.error = true;
+        scene.data.loading = false;
+      });
+  },
+  draw (canvas, frame) {
+    const { error, loading, value } = scene.data;
+
+    if (loading) {
+      return drawLoading(canvas, frame);
+    }
+
+    if (error) {
+      return canvas.fill([255, 0, 0]);
+    }
+
+    const halfLength = Math.floor(canvas.getLength() / 2);
+    const centre = halfLength;
+
+    const diffValue = (value.currentPrice - value.previousClosePrice) / value.previousClosePrice;
+    const diffPctLightCount = Math.abs(Math.floor(halfLength * diffValue));
+
+    canvas.clear();
+
+    if (diffValue > 0) {
+      canvas.gradient(centre, centre + diffPctLightCount, [0, 150, 0], [0, 255, 0]);
+    } else {
+      canvas.gradient(centre - diffPctLightCount, centre, [255, 0, 0], [150, 0, 0]);
+    }
+
+  }
+};
