cb4d2bd852bf26e10d644fe106f3dc64ed67667f
Alex Coulcher <alex@polyglot.rodeo>
2021-02-01 17:41:56 +0000

    fix: Remove updater

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,10 +4,11 @@ module.exports = {
       name: "l1ght-str1p",
       script: "index.js"
     },
-    {
-      name: "l1ght-str1p-update-daemon",
-      script: "update.sh",
-      cron: "*/15 * * * *"
-    }
+    // {
+    //   name: "l1ght-str1p-update-daemon",
+    //   script: "update.sh",
+    //   autorestart: false,
+    //   cron_restart: "*/15 * * * *"
+    // }
   ]
 }
