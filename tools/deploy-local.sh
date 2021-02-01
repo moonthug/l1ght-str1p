0daba69c7a2ff021437ca85c7a81f466228201e2
Alex Coulcher <alex@polyglot.rodeo>
2021-02-01 17:41:39 +0000

    fix: Add cleanup

--- tools/deploy-local.sh
+++ tools/deploy-local.sh
@@ -1,9 +1,24 @@
 #!/bin/bash
 
 APP_NAME="l1ght-str1p"
-HOST_NAME="l1ght-str1p.local"
+HOST_NAME="192.168.1.30"
 INSTALL_PARENT="~"
 INSTALL_DIR="$INSTALL_PARENT/$APP_NAME"
+
+#######################################
+# TRAPS
+#
+
+function cleanup {
+  echo "Cleaning up..."
+  rm deploy.tar.gz
+}
+
+trap cleanup EXIT
+
+#######################################
+# MAIN
+#
 
 # npm run build
 
@@ -11,12 +26,13 @@ echo 'Compressing build...'
 tar -czf deploy.tar.gz ./dist
 
 echo 'Cleaning install directory...'
-ssh pi@l1ght-str1p.local "sudo rm -rf $INSTALL_DIR"
+ssh "pi@$HOST_NAME" "sudo rm -rf $INSTALL_DIR"
+
 echo 'Copying build artifact...'
 scp deploy.tar.gz "pi@$HOST_NAME:deploy.tar.gz"
-echo 'Extracting deploy package...'
-ssh pi@l1ght-str1p.local "tar -zxf deploy.tar.gz && mv dist $APP_NAME"
-echo 'Starting application...'
-ssh pi@l1ght-str1p.local "cd $APP_NAME && sudo pm2 restart ecosystem.config.js"
 
-rm deploy.tar.gz
+echo 'Extracting deploy package...'
+ssh "pi@$HOST_NAME" "tar -zxf deploy.tar.gz && mv dist $APP_NAME"
+
+echo 'Starting application...'
+ssh "pi@$HOST_NAME" "cd $APP_NAME && sudo pm2 restart ecosystem.config.js"
